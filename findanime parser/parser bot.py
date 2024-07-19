d5def58bc84862ed155849fda53cdaa434ba10d4
Aksey010 <143290412+Aksey010@users.noreply.github.com>
2024-07-19 16:24:06 +0300

    Add files via upload

---
+++ findanime parser/parser bot.py
@@ -0,0 +1,54 @@
+import requests
+import pprint
+import telebot
+import time
+import pickle
+import random
+
+with open('report.txt', mode='rb') as f:
+    info = pickle.load(f)
+
+TOKEN = '7437356260:AAGtVvrUvfzcPulqSoxrgkwFTknB9dwWvHw'
+
+url = f'https://api.telegram.org/bot{TOKEN}/getMe'
+
+proxy ={
+     'http': 'http//:8.222.249.190:80'
+}
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+          }
+
+telebot.apihelper.proxy = {'http': 'http//:8.222.249.190:80'}
+
+bot = telebot.TeleBot(TOKEN)
+
+results = requests.get(url, headers=headers, proxies=proxy)
+
+url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+
+
+
+while True:
+    time.sleep(5)
+    results = requests.get(url, headers=headers, proxies=proxy)
+
+    pprint.pprint(results.json())
+
+    @bot.message_handler(commands=['help'])
+    def show_commands(message):
+        text = ''
+        for i in bot.get_my_commands():
+            text += str(i.command) + ' — ' + str(i.description) + '\n'
+        bot.send_message(message.chat.id, text=text)
+
+
+    @bot.message_handler(commands=['start'])
+    def random(message):
+        r_index = random.randint(0, 8325)
+        text =f'Название:\n{info[r_index][0]}\n\nОписание:\n{info[r_index][1]}\n\nСсылка:\n{info[r_index][2]}'
+        bot.send_message(message.chat.id, text=text)
+
+    bot.polling()
+
